69659c4b51dd101dac0058686bf272a281ccb27b
Per Andersson <per@westcoastcode.se>
2022-08-06 11:02:51 +0200

    Started using list

--- src/vmp/vmp_list_inherited_by.h
+++ src/vmp/vmp_list_inherited_by.h
@@ -2,30 +2,10 @@
 #define _VMP_LIST_INHERITED_BY_H_
 
 #include "../vm_string.h"
+#include "../vm_list.h"
 
-// List
-struct vmp_list_inherited_by
-{
-	vm_int32 count;
-	vm_int32 capacity;
-	struct vmp_type** memory;
-};
-typedef struct vmp_list_inherited_by vmp_list_inherited_by;
-
-// Initialize the list
-extern BOOL vmp_list_inherited_by_init(vmp_list_inherited_by* l);
-
-// Release the internal memory of list
-extern void vmp_list_inherited_by_release(vmp_list_inherited_by* l);
-
-// Add the supplied type
-extern vm_int32 vmp_list_inherited_by_add(vmp_list_inherited_by* l, struct vmp_type* ptr);
-
-// Search for a type with the supplied name
-extern struct vmp_type* vmp_list_inherited_by_find(const vmp_list_inherited_by* l, const vm_string* name);
-
-// Get a type of a specific index
-extern struct vmp_type* vmp_list_inherited_by_get(const vmp_list_inherited_by* l, vm_int32 index);
+DECLARE_LIST_TYPE(vmp_list_inherited_by, vmp_type);
+DECLARE_LIST_TYPE_FIND(vmp_list_inherited_by, vmp_type);
 
 // check to see if the supplied list contains the supplied item
 extern BOOL vmp_list_inherited_by_contains(const vmp_list_inherited_by* l, const struct vmp_type* ptr);
